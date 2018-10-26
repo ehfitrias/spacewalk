f7a119c72787910d6e93d59002da63539a322b31
ehfitrias <43806677+ehfitrias@users.noreply.github.com>
2018-10-26 09:56:36 +0700

    few revision on variables

--- spw_client.sh
+++ spw_client.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
-#Assume Spacewalk Server IP 10.197.17.208
 
-echo "Adding SpaceWalk Server to /etc/hosts"
+## setup Variables
+read -rp "Spacewalk IP Address: ($IP_SVR): " choice;
+	if [ "$choice" != "" ] ; then
+		export IP_SVR="$choice";
+	fi
+
+read -rp "Spacewalk Hostname: ($HOST_SVR): " choice;
+	if [ "$choice" != "" ] ; then
+		export HOST_SVR="$choice";
+	fi
+	
+read -rp "Spacewalk User: ($USER_SVR): " choice;
+	if [ "$choice" != "" ] ; then
+		export USER_SVR="$choice";
+	fi
+	
+read -rp "Spacewalk Password: ($PASS_SVR): " choice;
+	if [ "$choice" != "" ] ; then
+		export PASS_SVR="$choice";
+	fi
+
+## adding Spacewalk Server to /etc/hosts
 cat << EOF >> /etc/hosts
-10.197.17.208   bdi-poc-spw2
+$IP_SVR   $HOST_SVR
 EOF
 
-echo "Create Spacewalk Repo"
+## create Spacewalk Repo
 cat <<EOD > /etc/yum.repos.d/spacewalk.repo
 
 [spacewalk-client]
 name=Spacewalk Client
-baseurl=http://10.197.17.208/pub/spacewalk-client/
+baseurl=http://$IP_SVR/pub/spacewalk-client/
 enabled=1
 gpgcheck=0
 EOD
 
-echo "Install Packages & Registration"
+## install Packages & Registration
 yum -y install rhn-client-tools rhn-check rhn-setup rhnsd m2crypto yum-rhn-plugin
-rpm -Uvh http://10.197.17.208/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm
-rhnreg_ks --force --serverUrl https://bdi-poc-spw2/XMLRPC --sslCACert /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT --activationkey 1-b1e505ff7deff1539ba65720d37860e8 --profilename=$HOSTNAME
+rpm -Uvh http://$IP_SVR/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm
+rhnreg_ks --force --serverUrl https://$HOST_SVR/XMLRPC\
+--sslCACert /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT\
+--activationkey 1-b1e505ff7deff1539ba65720d37860e8\
+--profilename=$HOSTNAME
 rhn-profile-sync
 
-#delete spacewalk repo
+# delete spacewalk repo
 rm -rf /etc/yum.repos.d/spacewalk.repo
 
-#adding spacewalk client & epel channel
-spacewalk-channel -a -c spacewalk-client -c epel-7 -u admin -p P@ssw0rd
+# adding spacewalk client & epel channel
+spacewalk-channel -a -c spacewalk-client -c epel-7 -u $USER_SVR -p $PASS_SVR
 
-#install & configure OSAD
+# install & configure OSAD
 yum -y --nogpgcheck install osad rhncfg-actions
 systemctl | grep "osad.*running"
 if [ $? = 1 ]; then
@@ -36,10 +59,10 @@ if [ $? = 1 ]; then
 	systemctl enable osad
 fi
 
-#remove spacewalk client and epel channel
-spacewalk-channel -r -c spacewalk-client -c epel-7 -u admin -p P@ssw0rd
+# remove spacewalk client and epel channel
+spacewalk-channel -r -c spacewalk-client -c epel-7 -u $USER_SVR -p $PASS_SVR
 
-#enable checking system
+# enable checking system
 chcon system_u:object_r:rpm_exec_t:s0 /sbin/rhn_check-2.7
 
 #enable deploy & run
